c6f2ed6ae65f2b7232dce21127c4ab73412ec267
BadMax-Kirov <62816343+BadMax-Kirov@users.noreply.github.com>
2021-02-03 11:16:21 +0300

    Макс
    В работе

--- addProduct.php
+++ addProduct.php
@@ -4,22 +4,22 @@
     $db = new DatabaseShell('localhost', 'root', 'root', 'catalog');
   
     echo $form = '<div class="main">
-        <form method="POST" action="" class = "input">
-            <div class="field">
-                <label>Наименование товара</label>
-                <input type="text" name = "title">
-            </div><br>
-            <div class="field">
-                <label>Код товара</label>
-                <input type="text" name = "product_code">
-            </div><br>
-            <div class="field">
-                <label>Цена товара</label>
-                <input type="text" name = "price">
-            </div><br>
-        <input type="submit" value="Сохранить данные">
-    </form>   
-    </div>';
+                    <form method="POST" action="" class = "input">
+                        <div class="field">
+                            <label>Наименование товара</label>
+                            <input type="text" name = "title">
+                        </div><br>
+                        <div class="field">
+                            <label>Код товара</label>
+                            <input type="text" name = "product_code">
+                        </div><br>
+                        <div class="field">
+                            <label>Цена товара</label>
+                            <input type="text" name = "price">
+                        </div><br>
+                    <input type="submit" value="Сохранить данные">
+                </form>   
+                </div>';
    
     if(isset($_POST['title']) AND isset($_POST['product_code']) AND isset($_POST['price'])){
         if (empty($_POST['title']) AND empty($_POST['product_code']) AND empty($_POST['price'])){
@@ -34,6 +34,7 @@
             newProduct($db, $title, $product_code, $price);
           }
     }
+    
     function newProduct($db, $title, $product_code, $price)
     {
         $db->save('products', ['title' => $title, 'product_code' => $product_code, 'price' => $price]);
