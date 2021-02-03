4acac339d6ac41d1873da1f103138d4adcaeca75
BadMax-Kirov <62816343+BadMax-Kirov@users.noreply.github.com>
2021-02-03 08:37:31 +0300

    Макс
    в работе

--- addProduct.php
+++ addProduct.php
@@ -21,16 +21,18 @@
     </form>   
     </div>
 
- <?php   
+<?php   
     if(isset($_POST['title']) AND isset($_POST['product_code']) AND isset($_POST['price'])){
-        if (empty($_POST['title']) AND empty($_POST['product_code']) AND empty($_POST['price'])){?>
-            <script>alert('ќшибка! ¬ведены не все данные!')</script><?php
+        if (empty($_POST['title']) AND empty($_POST['product_code']) AND empty($_POST['price'])){
+?>
+            <script>alert('ќшибка! ¬ведены не все данные!')</script>
+<?php
         } else {
             $title = $_POST['title'];
             $product_code = $_POST['product_code'];
             $price = $_POST['price'];
         
-        newProduct($db, $title, $product_code, $price);
+            newProduct($db, $title, $product_code, $price);
           }
     }
     function newProduct($db, $title, $product_code, $price)
